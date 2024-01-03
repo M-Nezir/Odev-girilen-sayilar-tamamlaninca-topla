a458044878ceddeadda439bc3e1bdfc11dcb7621
M-Nezir <146748647+M-Nezir@users.noreply.github.com>
2024-01-03 11:53:19 +0300

    Update Program.cs

--- Odev=girilen sayilar tamamlaninca topla/Program.cs
+++ Odev=girilen sayilar tamamlaninca topla/Program.cs
@@ -10,42 +10,27 @@ namespace Odev_girilen_sayilar_tamamlaninca_topla
     {
         static void Main(string[] args)
         {
-            /*ÖDEV: Öyle bir program yazın ki, önce kullanıcıya kaç adet sayı girmek istediğini sorsun.
-               Daha sonra kullanıcıdan sayı girmesini istesin. Kullanıcı fark etse de etmese de baştan 
-             girdiği adet kadar sayı tamamlandığında bu sayıların toplamını ve ortalamasını hesaplayarak ekrana yazsın.*/
+            //Bu program kullanıcıdan kaç tane sayı girmek istediğini sorar.
+//istenilen adette sayı girilince bunların hem toplamını hem ortalamasını yazar.
 
+ Console.WriteLine("kaç adet sayıyı toplamak istersiniz: ");
+ int adet = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("kaç adet sayıyı toplamak istersiniz: ");
-            int adet = Convert.ToInt32(Console.ReadLine());
+ double toplam = 0;
+ for (int i = 1; adet >= i; i++)
+ {
+     Console.WriteLine($"lütfen toplamak istediğiniz sayılardan {i}. sayıyı giriniz:");
+     int sayılar = Convert.ToInt32(Console.ReadLine());
 
-            
+     toplam = sayılar + toplam;                
+ }
+ 
+ Console.WriteLine($"yazdığınız sayıların toplamı: {toplam}");
+ Console.WriteLine($"yazdığnız sayıların ortalaması: {toplam / adet}");
 
-           double toplam = 0 , newtoplam = 0;
+ Console.WriteLine("bitirmek için enter: ");
+ Console.ReadLine();
 
-            for (int i = 0; adet - 1 >= i; i++)
-            {
-
-                
-                Console.WriteLine($"lütfen toplamak istediğiniz sayılardan {i+1}. sayıyı giriniz:");
-                int sayılar = Convert.ToInt32(Console.ReadLine());
-
-                newtoplam = sayılar + toplam;
-                toplam = newtoplam;
-
-                
-                if (i != adet-1)
-                {
-                    continue;
-                }
-
-                Console.WriteLine($"yazdığınız sayıların toplamı: {toplam}");
-                Console.WriteLine($"yazdığnız sayıların ortalaması: {toplam/adet}");
-
-                Console.WriteLine("bitirmek için enter: ");
-                Console.ReadLine();
-
-                break;
-            }
 
         }
     }
